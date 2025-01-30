3b6bb320914ad7a829733a72a641190c71c9c402
Matti Hernesniemi <matti.hernesniemi@exove.fi>
2025-01-30 12:19:13 +0200

    Revert "add es"
    This reverts commit 29b23cd5bce310b0a111356ad487a981730d5eac.

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,18 +1,35 @@
+import request from "graphql-request";
+import { API_URL } from "@/common/constants";
+import { productsQuery } from "@/common/queries";
 import Container from "@/components/container";
 import Header from "@/components/header";
-import ListingTemplate from "./templates/listing-template";
+import ProductCard from "@/components/product-card";
 
 type Params = Promise<{ locale: string }>;
 
 export default async function Home(props: { params: Params }) {
   const { locale: languageCode } = await props.params;
-
-  console.log("languageCode", languageCode);
+  const data = await request(
+    `${API_URL}?languageCode=${languageCode}`,
+    productsQuery,
+  );
 
   return (
     <Container>
       <Header />
-      <ListingTemplate />
+      <div className="mx-auto max-w-screen-2xl py-20">
+        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {data.products.items.map((product) => (
+            <ProductCard
+              key={product.id}
+              slug={product.slug}
+              name={product.name}
+              imageSource={product.assets[0]?.source ?? ""}
+              priceWithTax={product.variantList.items[0].priceWithTax}
+            />
+          ))}
+        </div>
+      </div>
     </Container>
   );
 }
