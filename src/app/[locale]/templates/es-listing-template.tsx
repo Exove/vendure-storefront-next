d30f8222c2de150af2ee32e8123df0b496d1354e
Matti Hernesniemi <matti.hernesniemi@exove.fi>
2025-03-08 15:19:55 +0200

    init useRefinementList

--- src/app/[locale]/templates/es-listing-template.tsx
+++ src/app/[locale]/templates/es-listing-template.tsx
@@ -2,6 +2,57 @@
 
 import { InstantSearch, SearchBox, Hits } from "react-instantsearch";
 import createClient from "@searchkit/instantsearch-client";
+import { useRefinementList } from "react-instantsearch";
+import React from "react";
+
+// Mukautettu komponentti RefinementList-suodattimelle käyttäen hookia
+interface CustomRefinementListProps {
+  title: string;
+  attribute: string;
+}
+
+const CustomRefinementList = ({
+  title,
+  attribute,
+}: CustomRefinementListProps) => {
+  const { items, refine } = useRefinementList({
+    attribute,
+    limit: 20,
+  });
+
+  return (
+    <div>
+      <h3>{title}</h3>
+
+      {/* Suodatinvaihtoehdot */}
+      <div>
+        <ul style={{ maxHeight: "200px", overflowY: "auto" }}>
+          {items.map((item) => (
+            <li key={item.label}>
+              <label style={{ display: "flex", cursor: "pointer" }}>
+                <input
+                  type="checkbox"
+                  checked={item.isRefined}
+                  onChange={() => refine(item.value)}
+                />
+                <span>
+                  {item.highlighted ? (
+                    <span
+                      dangerouslySetInnerHTML={{ __html: item.highlighted }}
+                    />
+                  ) : (
+                    item.label
+                  )}
+                </span>
+                <span style={{ marginLeft: "auto" }}>({item.count})</span>
+              </label>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 const searchClient = createClient({
   url: "/api/search",
@@ -10,8 +61,27 @@ const searchClient = createClient({
 export default function Search() {
   return (
     <InstantSearch searchClient={searchClient} indexName="vendure-variants">
-      <SearchBox />
-      <Hits />
+      <div>
+        <div>
+          <div>
+            <h2>Suodata</h2>
+            <CustomRefinementList
+              title="Kategoriat"
+              attribute="product-facetValueSlugs"
+            />
+            <CustomRefinementList
+              title="Brändit"
+              attribute="product-facetValueSlugs"
+            />
+          </div>
+        </div>
+        <div>
+          <div>
+            <SearchBox placeholder="Etsi tuotteita..." />
+          </div>
+          <Hits />
+        </div>
+      </div>
     </InstantSearch>
   );
 }
