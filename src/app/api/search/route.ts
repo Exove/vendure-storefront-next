dd7a7d4e32cd5f609fa9a5c8913cc271dd2c6ffe
Mikko Nirkko <mikkonirkko@Mikkos-MacBook-Pro.local>
2025-03-11 12:27:29 +0200

    include auth in ES config

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -4,6 +4,10 @@ import { NextRequest, NextResponse } from "next/server";
 const apiConfig = {
   connection: {
     host: process.env.NEXT_PUBLIC_ELASTICSEARCH_HOST || "http://localhost:9200",
+    auth: {
+         username: process.env.ELASTICSEARCH_USERNAME,
+         password: process.env.ELASTICSEARCH_PASSWORD
+      },
   },
   search_settings: {
     analysis: {
